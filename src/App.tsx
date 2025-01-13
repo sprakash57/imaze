ffc9a2e72a3aaaf442cd09089e3eaab718314204
Sunny Prakash <sunny.prakashgm@gmail.com>
2025-01-13 19:36:45 +0100

    refactor: integrate pexels api to simple photo grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import getCuratedPhotos, { CuratedPhoto } from "./utils/api";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = styled.div`
+  max-width: 85%;
+  margin: 20px auto;
+`;
+
+const ImageContainer = styled.div`
+  columns: 3 250px;
+  gap: 15px;
+
+  img {
+    margin-bottom: 10px;
+    border-radius: 5px;
+    width: 100%;
+  }
+`;
+
+const App: React.FC = () => {
+  const [photos, setPhotos] = useState<CuratedPhoto[]>([]);
+
+  const loadPhotos = async () => {
+    const data = await getCuratedPhotos();
+    setPhotos((photos) => [...photos, ...data.photos]);
+  };
+
+  useEffect(() => {
+    loadPhotos();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container>
+      <ImageContainer>
+        {photos.map(({ src, id }, index) => (
+          <img key={id} src={src.medium} data-index={index} />
+        ))}
+      </ImageContainer>
+    </Container>
+  );
+};
 
-export default App
+export default App;
---
+++ src/utils/api.ts
@@ -0,0 +1,54 @@
+export interface CuratedPhoto {
+  id: number;
+  width: number;
+  height: number;
+  url: string;
+  photographer: string;
+  photographer_url: string;
+  photographer_id: number;
+  avg_color: string;
+  src: {
+    original: string;
+    large2x: string;
+    large: string;
+    medium: string;
+    small: string;
+    portrait: string;
+    landscape: string;
+    tiny: string;
+  };
+  liked: boolean;
+  alt: string;
+}
+
+export interface GetCuratedPhotosResponse {
+  page: number;
+  per_page: number;
+  photos: CuratedPhoto[];
+  total_results: number;
+  next_page: string;
+}
+
+export default async function getCuratedPhotos(
+  page: number = 1,
+  perPage: number = 15
+): Promise<GetCuratedPhotosResponse> {
+  const response = await fetch(
+    `${
+      import.meta.env.VITE_PEXELS_API_URL
+    }/curated?page=${page}&per_page=${perPage}`,
+    {
+      headers: {
+        Authorization: import.meta.env.VITE_PEXELS_API_KEY,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch photos");
+  }
+
+  const data: GetCuratedPhotosResponse = await response.json();
+
+  return data;
+}
